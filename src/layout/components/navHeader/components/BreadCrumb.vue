1ed94ae671d917e4e7f76c099b338a9702b10e39
曾根发 <zenggenfa@xd.com>
2021-12-17 18:43:28 +0800

    feat: compelete breadcrumb component

--- src/layout/components/navHeader/components/BreadCrumb.vue
+++ src/layout/components/navHeader/components/BreadCrumb.vue
@@ -9,22 +9,41 @@
   <div class="breadcrumb">
     <el-breadcrumb separator="/">
       <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-      <el-breadcrumb-item>活动管理</el-breadcrumb-item>
-      <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-      <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+      <el-breadcrumb-item v-for="(item, index) in routeMatched" :key="index" :to="item.path">
+        {{ item.meta.title }}
+      </el-breadcrumb-item>
     </el-breadcrumb>
   </div>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue'
-  export default defineComponent({
-    name: 'BreadCrumb',
-  })
+import { defineComponent, watch, ref } from 'vue'
+import { RouteLocationMatched, useRoute } from 'vue-router'
+export default defineComponent({
+  name: 'BreadCrumb',
+  setup() {
+    const route = useRoute()
+    let routeMatched = ref<RouteLocationMatched[]>([])
+
+    watch(
+      () => route.matched,
+      () => {
+        routeMatched.value = route.matched
+      },
+      {
+        immediate: true,
+        deep: true,
+      }
+    )
+    return {
+      routeMatched,
+    }
+  },
+})
 </script>
 
 <style lang="scss" scoped>
-  :deep(.el-breadcrumb__inner):hover {
-    color: $theme;
-  }
+:deep(.el-breadcrumb__inner):hover {
+  color: $theme;
+}
 </style>
